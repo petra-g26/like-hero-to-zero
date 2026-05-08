7d851603ccc78f89203ae350d290b8c6db83687d
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-08 20:10:09 +0200

    emission Entity und CO2 test Daten hinzugefügt

--- src/main/java/de/petragawellek/likeherotozero/config/DataLoader.java
+++ src/main/java/de/petragawellek/likeherotozero/config/DataLoader.java
@@ -4,22 +4,35 @@ import de.petragawellek.likeherotozero.model.Country;
 import de.petragawellek.likeherotozero.repository.CountryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import de.petragawellek.likeherotozero.model.Emission;
+import de.petragawellek.likeherotozero.repository.EmissionRepository;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final CountryRepository countryRepository;
+    private final EmissionRepository emissionRepository;
 
-    public DataLoader(CountryRepository countryRepository) {
+    public DataLoader(CountryRepository countryRepository,
+                      EmissionRepository emissionRepository) {
         this.countryRepository = countryRepository;
+        this.emissionRepository = emissionRepository;
     }
 
     @Override
     public void run(String... args) {
-
-        countryRepository.save(new Country("Deutschland"));
-        countryRepository.save(new Country("Frankreich"));
-        countryRepository.save(new Country("USA"));
+        Country deutschland =
+                countryRepository.save(new Country("Deutschland"));
+        Country frankreich =
+                countryRepository.save(new Country("Frankreich"));
+        Country usa =
+                countryRepository.save(new Country("USA"));
+        emissionRepository.save(
+                new Emission(2022, 600.5, deutschland));
+        emissionRepository.save(
+                new Emission(2022, 450.2, frankreich));
+        emissionRepository.save(
+                new Emission(2022, 1200.8, usa));
 
     }
 }
---
+++ src/main/java/de/petragawellek/likeherotozero/repository/EmissionRepository.java
@@ -0,0 +1,5 @@
+package de.petragawellek.likeherotozero.repository;
+import de.petragawellek.likeherotozero.model.Emission;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EmissionRepository extends JpaRepository<Emission, Long> {
+}
