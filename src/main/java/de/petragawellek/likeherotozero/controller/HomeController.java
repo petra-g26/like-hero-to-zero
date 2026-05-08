6be7bd1deec441f6597f7b84f88886881aacc309
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-08 19:50:14 +0200

    Verbunden mit der Datenbank und anzeigen der Länder

--- src/main/java/de/petragawellek/likeherotozero/controller/HomeController.java
+++ src/main/java/de/petragawellek/likeherotozero/controller/HomeController.java
@@ -1,13 +1,19 @@
 package de.petragawellek.likeherotozero.controller;
 
+import de.petragawellek.likeherotozero.repository.CountryRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
-
+    private final CountryRepository countryRepository;
+    public HomeController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("countries",
+                countryRepository.findAll());
         return "index";
     }
 }
