1e0a32a4144290f8c908c150a963f7bfac54c4ca
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-08 19:36:07 +0200

    Contry entity hinzugefügt

---
+++ src/main/java/de/petragawellek/likeherotozero/model/Country.java
@@ -0,0 +1,26 @@
+package de.petragawellek.likeherotozero.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    public Country() {
+    }
+    public Country(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
