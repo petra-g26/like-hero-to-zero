49c9f6729a42a3e9e024527ba75219ef4dbc1b6b
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-09 12:47:44 +0200

    Designaenderungen, wissenschaftlerbereich anpassen

--- src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
+++ src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
@@ -1,13 +1,25 @@
 package de.petragawellek.likeherotozero.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ScientistController {
 
     @GetMapping("/scientist")
     public String scientistPage() {
+
+        return "scientist";
+    }
+
+    @PostMapping("/scientist")
+    public String saveEmission(Model model) {
+
+        model.addAttribute(
+                "successMessage",
+                "CO₂-Daten wurden erfolgreich gespeichert.");
 
         return "scientist";
     }
