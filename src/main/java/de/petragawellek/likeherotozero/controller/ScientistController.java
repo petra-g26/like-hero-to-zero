e0664f21333f5a51f92a34f7ad73c846fd29a7e0
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-15 07:19:24 +0200

    Fehler behoben, letzter sauberer Code wieder hergestellt; neuer Versuch

--- src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
+++ src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
@@ -1,18 +1,20 @@
 package de.petragawellek.likeherotozero.controller;
-
+import de.petragawellek.likeherotozero.model.EmissionData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import de.petragawellek.likeherotozero.model.EmissionData;
 import java.util.ArrayList;
 import java.util.List;
-
 @Controller
 public class ScientistController {
     private final List<EmissionData> emissions = new ArrayList<>();
-
+    @GetMapping("/scientist")
+    public String scientistPage(Model model) {
+        model.addAttribute("emissions", emissions);
+        return "scientist";
+    }
     @PostMapping("/scientist")
     public String saveEmission(
             @RequestParam String country,
@@ -25,11 +27,6 @@ public class ScientistController {
         model.addAttribute(
                 "successMessage",
                 "CO₂-Daten wurden erfolgreich gespeichert.");
-        model.addAttribute("emissions", emissions);
-        return "scientist";
-    }
-    @GetMapping("/scientist")
-    public String scientistPage(Model model) {
         model.addAttribute("emissions", emissions);
         return "scientist";
     }
