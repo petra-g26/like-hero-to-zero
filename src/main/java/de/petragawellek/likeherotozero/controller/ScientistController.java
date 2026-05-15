a19f2edc75d66ca33275daea6da40a760908dfce
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-15 06:21:15 +0200

    Layout und Wissenschaftlerseite angepasst

--- src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
+++ src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
@@ -4,23 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ScientistController {
-
     @GetMapping("/scientist")
     public String scientistPage() {
-
         return "scientist";
     }
-
     @PostMapping("/scientist")
-    public String saveEmission(Model model) {
-
+    public String saveEmission(
+            @RequestParam String country,
+            @RequestParam Integer year,
+            @RequestParam Double co2,
+            Model model) {
         model.addAttribute(
                 "successMessage",
                 "CO₂-Daten wurden erfolgreich gespeichert.");
-
+        model.addAttribute("country", country);
+        model.addAttribute("year", year);
+        model.addAttribute("co2", co2);
         return "scientist";
     }
 }
