246e0548749d80c74d0fec9983acee3336e9d394
Petra Gawellek <petra.gawellek@iu-study.org>
2026-05-15 06:40:39 +0200

    Tabellenübersicht eingepflegt und funktioniert

--- src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
+++ src/main/java/de/petragawellek/likeherotozero/controller/ScientistController.java
@@ -5,25 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import de.petragawellek.likeherotozero.model.EmissionData;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ScientistController {
-    @GetMapping("/scientist")
-    public String scientistPage() {
-        return "scientist";
-    }
+    private final List<EmissionData> emissions = new ArrayList<>();
+
     @PostMapping("/scientist")
     public String saveEmission(
             @RequestParam String country,
             @RequestParam Integer year,
             @RequestParam Double co2,
             Model model) {
+        EmissionData data =
+                new EmissionData(country, year, co2);
+        emissions.add(data);
         model.addAttribute(
                 "successMessage",
                 "CO₂-Daten wurden erfolgreich gespeichert.");
-        model.addAttribute("country", country);
-        model.addAttribute("year", year);
-        model.addAttribute("co2", co2);
+        model.addAttribute("emissions", emissions);
+        return "scientist";
+    }
+    @GetMapping("/scientist")
+    public String scientistPage(Model model) {
+        model.addAttribute("emissions", emissions);
         return "scientist";
     }
 }
